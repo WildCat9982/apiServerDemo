05f525d3417f5c67f49d6715de7041be73f5eaeb
WildCat9982 <chekeong82@gmail.com>
2021-09-20 16:46:42 +0800

    Added endpoints /refresh-token /revoke-token

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 var path = require('path');
 const jwt = require('jsonwebtoken')
+const uuidv4 = require('uuid').v4;
+const moment = require('moment');
 
 const app = express()
 
@@ -28,44 +30,191 @@ const users = [
     }
 ]
 
+// a list to holds all refresh token issused to user
+var refreshTokens = []
 
-app.get('/api', (req, res) => {
-    res.json({
-        message: 'API Testing page'
-    })
-})
-
-app.post('/api/posts', verifyToken, (req, res) => {
-    jwt.verify(req.token, SECRET_KEY, (err, authData) => {
+app.post('/about', verifyToken, (req, res) => {
+    jwt.verify(req.token, SECRET_KEY, {ignoreExpiration: false}, (err, authData) => {
         if (err) {
-            res.sendStatus(403); // forbidden
+            res.status(401).json({message: 'Invalid Token'})
         } else {
-            res.json({
-                message: 'Posts created...',
-                authData
-
-            })
+            res.send('<h1>Hello World!</h1>')
         }
     })
 })
 
-app.get('/api/token', (req, res)=> {
+app.post('/revoke-token', (req, res) => {
+    try {
+        const {accessToken, refreshToken } = req.body;
+        jwt.verify(accessToken, SECRET_KEY, {ignoreExpiration: false}, (err, authData) => {
+            if (err) {
+                res.sendStatus(403); // forbidden
+            } else {
+
+                console.log('authData', authData)
+                // retrieve access token jwtId 
+                const acessTokenJwtId = authData['jti'];
+                console.log(acessTokenJwtId, refreshToken)
+
+                // check if the refresh token is linked to the acccess token
+                if(acessTokenJwtId !== refreshToken)
+                    throw Error("Token does not match with Refresh Token");
+
+                // search refresh token from the refresh token store
+                const oldRefreshToken = refreshTokens.find( (token) => token.jwtId === refreshToken);
+                if (!oldRefreshToken) 
+                    throw Error('refresh token does not exist');
+
+                if (oldRefreshToken.revoked || oldRefreshToken.used)
+                    throw Error ("Refresh Token has used or been revoked")
+
+                // check if the refresh token has expired:
+                if (moment().isAfter(oldRefreshToken.expiryDate))
+                    throw new Error("Refresh Token has expired")
+                
+                const userDto = oldRefreshToken.user;
+                console.log(userDto)
+                
+                // verified that refresh token is valid:
+                // create access token with 15mins expirydate
+                oldRefreshToken.revoked = true;
+                res.status(200).json({
+                    message: 'Token has been revoked successfully'
+                });
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+
+})
+
+app.post('/refresh-token', (req, res) => {
+    try {
+        const {accessToken, refreshToken } = req.body;
+        jwt.verify(accessToken, SECRET_KEY, {ignoreExpiration: false}, (err, authData) => {
+            if (err) {
+                res.sendStatus(403); // forbidden
+            } else {
+
+                console.log('authData', authData)
+                // retrieve access token jwtId 
+                const acessTokenJwtId = authData['jti'];
+                console.log(acessTokenJwtId, refreshToken)
+
+                // check if the refresh token is linked to the acccess token
+                if(acessTokenJwtId !== refreshToken)
+                    throw Error("Token does not match with Refresh Token");
+
+                // search refresh token from the refresh token store
+                const oldRefreshToken = refreshTokens.find( (token) => token.jwtId === refreshToken);
+                if (!oldRefreshToken) 
+                    throw Error('refresh token does not exist');
+
+                if (oldRefreshToken.revoked || oldRefreshToken.used)
+                    throw Error ("Refresh Token has used or been revoked")
+
+                // check if the refresh token has expired:
+                if (moment().isAfter(oldRefreshToken.expiryDate))
+                    throw new Error("Refresh Token has expired")
+                
+                const userDto = oldRefreshToken.user;
+                console.log(userDto)
+                // verified that refresh token is valid:
+                // create access token with 15mins expirydate
+                const jwtId = uuidv4();
+                const options = {
+                    expiresIn: "15m",
+                    jwtid: jwtId,    // specify jwtid (an id that token)  (needed for the refresh token, as a refresh token only points to one single unique token)
+                    subject: userDto.id.toString() // the subject should be the users id (primary key)
+                }
+
+                jwt.sign({user: userDto}, SECRET_KEY, options, (err, token) => {
+                    if (err) {
+                        res.sendStatus(403);
+                    } else {
+                        
+                        // set the old refresh token as been used
+                        oldRefreshToken.used = true;
+
+                        // create Refresh Token  with 7-days expriy and store it into RefreshtTokenList
+                        const newRefreshToken = {
+                            user: userDto, 
+                            jwtId: jwtId,
+                            expriyDate: moment().add(7, "d").toDate(),
+                            used: false,
+                            revoked: false
+                        }
+                        refreshTokens.push(newRefreshToken)
+                        console.log('refreshTokens length:', refreshTokens.length)
+                        console.log(refreshTokens[refreshTokens.length-1])
+                        res.json({
+                            token: {
+                                accessToken: token,
+                                refreshToken: newRefreshToken.jwtId
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
+app.get('/token', (req, res)=> {
     res.render("token", { title: "Login Page" });
 })
 
-app.post('/api/login', (req, res) => {
-    console.log('post login')
+app.post('/login', (req, res) => {
     const {username, password} = req.body;
-
     const user = users.find( (user) => user.username === username && user.password === password);
-    if (!user)
+    if (!user) {
         res.status(401).send('incorrect username or password')
+    }
+    else {
+        const jwtId = uuidv4();
+        const options = {
+            expiresIn: "15m",
+            jwtid: jwtId,    // specify jwtid (an id that token)  (needed for the refresh token, as a refresh token only points to one single unique token)
+            subject: user.id.toString() // the subject should be the users id (primary key)
+        }
 
-    jwt.sign({user: user}, SECRET_KEY, (err, token) => {
-        res.json({
-            token,
+        // create access token with 15mins expirydate
+        const userDto = {
+            id: user.id,
+            username: user.username,
+            role: user.role
+        }
+        jwt.sign({user: userDto}, SECRET_KEY, options, (err, token) => {
+            if (err) {
+                res.sendStatus(403);
+            } else {
+        
+                // create Refresh Token  with 7-days expriy and store it into RefreshtTokenList
+                const refreshToken = {
+                    user: userDto, 
+                    jwtId: jwtId,
+                    expriyDate: moment().add(7, "d").toDate(),
+                    revoked: false
+                }
+                refreshTokens.push(refreshToken)
+                console.log('refreshTokens length:', refreshTokens.length)
+                console.log(refreshTokens[refreshTokens.length-1])
+                res.json({
+                    token: {
+                        accessToken: token,
+                        refreshToken: refreshToken.jwtId
+                    }
+                })
+            }
         })
-    })
+    }
 })
 
 function verifyToken(req, res, next) {
